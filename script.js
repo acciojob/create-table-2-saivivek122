25b70e88d17aff64b575b117f9e05f0adf9552e0
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-15 05:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+let table=document.getElementById("myTable")
+
 function createTable() {
     //Write your code here
+	let rows=prompt("Input number of rows");
+	let columns =prompt("Input number of columns");
+
+	if((isNaN(rows) || isNaN(columns))|| (parseInt(rows)<0 || parseInt(columns)<0)){
+		return;
+	}
+
+	for(let i=0;i<parseInt(rows);i++){
+		let tr=table.insertRow();
+		for(let j=0;j<parseInt(columns);j++){
+			let td=tr.insertCell();
+			td.textContent="Row-"+i+" "+"Column-"+j
+		}
+	}
   
 }
